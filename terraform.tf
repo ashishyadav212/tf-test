b3a8fd960cdee68d4c5d83fcb9dfbf00e18a3402
ashishyadav212 <ashishyadavg02@gmail.com>
2026-01-04 18:36:20 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.92"
+    google = {
+      source  = "hashicorp/google"
+      version = "> 5.92"
     }
   }
 
